5b66157bc1e02769278d7fe599b4e5ad0efcb91e
nosrednawall <nosrednawall@gmail.com>
2019-02-26 15:24:43 -0300

    iniciado o component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,26 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { PhotoService } from './photos/photo/photo.service';
-import { Photo } from './photos/photo/photo';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent implements OnInit {
-  title = 'alurapic';
-
-  // tslint:disable-next-line:ban-types
-  photos: Photo[] = [];
-
-  constructor(private photoService: PhotoService) { }
-
-  ngOnInit(): void {
-
-    this.photoService.listFromUser('flavio')
-      .subscribe(
-        photos => this.photos = photos
-      );
-  }
-}
+export class AppComponent {}
